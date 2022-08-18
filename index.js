a27a8e81c73a463a857b825f764024354b4ab7bc
TaniaSem <semenchuktetiana@gmail.com>
2022-08-19 00:12:29 +0300

    add start files

---
+++ index.js
@@ -0,0 +1,56 @@
+const refs = {
+  timerFace: document.querySelector(".timer"),
+  days: document.querySelector("[data-days]"),
+  hours: document.querySelector("[data-hours]"),
+  minutes: document.querySelector("[data-minutes]"),
+  seconds: document.querySelector("[data-seconds]"),
+};
+
+let deltaTime;
+
+const intervalId = setInterval(() => {
+  const startTime = 1664985600000;
+  deltaTime = startTime - Date.now();
+  const timeComponents = convertMs(deltaTime);
+  console.log(timeComponents);
+  updateTimerFace(timeComponents);
+  onStopTimer();
+}, 1000);
+
+function onStopTimer() {
+  if (deltaTime === 0 || deltaTime <= 1000) {
+    clearInterval(intervalId);
+  }
+}
+
+console.log(Date.now());
+
+function updateTimerFace({ days, hours, minutes, seconds }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${minutes}`;
+  refs.seconds.textContent = `${seconds}`;
+}
+
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = pad(Math.floor(ms / day));
+  // Remaining hours
+  const hours = pad(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
